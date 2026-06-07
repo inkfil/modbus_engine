edbd4161cb0d116f0624279234c7045fc1ca25bc
inkfil <ronakjainrjrjrj@gmail.com>
2026-06-07 15:30:12 +0530

    added: vcpkg.json for dependency management for github actions

--- include/version.h
+++ include/version.h
@@ -2,6 +2,7 @@
 #define VERSION_H_IN
 
 // the configured options and settings for the project
+#define PROJECT_BUILD_PLATFORM Darwin
 #define PROJECT_VERSION_MAJOR 1
 #define PROJECT_VERSION_MINOR 2
 #define PROJECT_VERSION_PATCH 
@@ -9,7 +10,7 @@
 #define PROJECT_NAME_STRING "modbus_engine"
 #define BUILD_TYPE ""
 #define CMAKE_SYSTEM_NAME "Darwin"
-#define BUILD_TIMESTAMP "2026-05-30 09:39:59"
-#define GIT_HASH "af0d7d1"
+#define BUILD_TIMESTAMP "2026-06-07 15:28:01"
+#define GIT_HASH "537a4af"
 
 #endif // VERSION_H_IN
